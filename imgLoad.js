79b4987e8a5b028898d2d0649bda424e2bb3c197
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2024-09-03 22:18:17 -0700

    fix image loader

--- imgLoad.js
+++ imgLoad.js
@@ -1,4 +1,4 @@
 // https://stackoverflow.com/questions/76122243/the-image-path-is-weird-and-i-cannot-see-any-image-after-deploying-my-next-js-ap
 export default function ImageLoader({ src }) {
-    return `/hodytek-deploy/${src}`;
+    return `https://zhangvicto.github.io/hodytek-deploy/${src}`;
 }
