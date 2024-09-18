aa407248c8ed764fb0f3e83d77fb0f315e6447f5
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2024-09-17 23:58:54 -0700

    redeploy

--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  // basePath: "/hodytek-deploy",
+  basePath: "/hodytek-deploy",
 
   /**
    * Disable server-based image optimization. Next.js does not support
@@ -21,9 +21,9 @@ const nextConfig = {
    * @see https://nextjs.org/docs/app/api-reference/components/image#unoptimized
    */
   images: {
-    unoptimized: true,
-    // loader: "custom",
-    // loaderFile: './imgLoad.js'
+    // unoptimized: true,
+    loader: "custom",
+    loaderFile: './imgLoad.js'
   },
 };
 
