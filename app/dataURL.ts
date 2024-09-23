1650f05356fe7d3191d8a286af8e3f84f34a93d3
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2024-09-22 22:34:48 -0700

    update

--- app/dataURL.ts
+++ app/dataURL.ts
@@ -1,12 +1,12 @@
 export default function publicDataURL(filename:string) {
-    const deployment = true;
+    const deployment = false;
     if (deployment) {
         return "https://zhangvicto.github.io/hodytek-deploy/" + filename
     } else return "/" + filename;
 }
 
 export function publicPDFURL(filename:string) {
-    const deployment = true;
+    const deployment = false;
     if (deployment) {
         return "https://zhangvicto.github.io/hodytek-deploy/" + filename
     } else return filename;
--- next.config.mjs
+++ next.config.mjs
@@ -12,7 +12,7 @@ const nextConfig = {
    *
    * @see https://nextjs.org/docs/app/api-reference/next-config-js/basePath
    */
-  basePath: "/hodytek-deploy",
+  // basePath: "/hodytek-deploy",
 
   /**
    * Disable server-based image optimization. Next.js does not support
@@ -22,8 +22,8 @@ const nextConfig = {
    */
   images: {
     // unoptimized: true,
-    loader: "custom",
-    loaderFile: './imgLoad.js'
+    // loader: "custom",
+    // loaderFile: './imgLoad.js'
   },
 };
 
