b3b03c4d95889ede7c7e60014214ab91fe4aee68
Victor Zhang <64699812+zhangvicto@users.noreply.github.com>
2024-09-22 22:12:12 -0700

    update

--- app/dataURL.ts
+++ app/dataURL.ts
@@ -1,12 +1,12 @@
 export default function publicDataURL(filename:string) {
-    const deployment = false;
+    const deployment = true;
     if (deployment) {
         return "https://zhangvicto.github.io/hodytek-deploy/" + filename
     } else return "/" + filename;
 }
 
 export function publicPDFURL(filename:string) {
-    const deployment = false;
+    const deployment = true;
     if (deployment) {
         return "https://zhangvicto.github.io/hodytek-deploy/" + filename
     } else return filename;
